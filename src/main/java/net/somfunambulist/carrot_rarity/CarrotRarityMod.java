6713b4fbfa3e89e5c88f068e247f020bc42fdc6d
somfunambulist <mccoymylesd@gmail.com>
2025-01-11 16:57:14 -0500

    composter support and farmers delight compat

--- src/main/java/net/somfunambulist/carrot_rarity/CarrotRarityMod.java
+++ src/main/java/net/somfunambulist/carrot_rarity/CarrotRarityMod.java
@@ -52,7 +52,7 @@ public class CarrotRarityMod {
             event.accept(ModItems.TWISTY_CARROT);
             event.accept(ModItems.LEGGY_CARROT);
             event.accept(ModItems.CARROT_BABY);
-            event.accept(ModItems.HOLOGRAPHIC_CARROT_BABY);
+            //event.accept(ModItems.HOLOGRAPHIC_CARROT_BABY);
         }
     }
 
